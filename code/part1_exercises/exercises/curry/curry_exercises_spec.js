09a871688a72ceb54b39a1e80eb22875c469b228
James Lelyveld <james.lelyveld@tribalworldwide.co.uk>
2017-01-20 11:25:11 +0000

    Exercise 1 inc re-writing tests for ES6

--- code/part1_exercises/exercises/curry/curry_exercises_spec.js
+++ code/part1_exercises/exercises/curry/curry_exercises_spec.js
@@ -1,34 +1,58 @@
 var E = require('./curry_exercises');
 var assert = require("chai").assert;
 
-describe("Curry Exercises", function(){
+describe ("Curry Exercises", () => {
+  
+    it (
+        'Exercise 1', 
+        () => assert.deepEqual(
+            E.words("Jingle bells Batman smells"), 
+            ['Jingle', 'bells', 'Batman', 'smells']
+        )
+    );
+  
 
-  it('Exercise 1', function(){
-    assert.deepEqual(E.words("Jingle bells Batman smells"), ['Jingle', 'bells', 'Batman', 'smells']);
-  });
+    it (
+        'Exercise 1a', 
+        () => assert.deepEqual(
+            E.sentences(["Jingle bells Batman smells", "Robin laid an egg"]), 
+            [['Jingle', 'bells', 'Batman', 'smells'], ['Robin', 'laid', 'an', 'egg']]
+        )
+    );
 
-  it('Exercise 1a', function(){
-    assert.deepEqual(E.sentences(["Jingle bells Batman smells", "Robin laid an egg"]), [['Jingle', 'bells', 'Batman', 'smells'], ['Robin', 'laid', 'an', 'egg']]);
-  });
+    it(
+        'Exercise 2', 
+        () => assert.deepEqual(
+            E.filterQs(['quick', 'camels', 'quarry', 'over', 'quails']), 
+            ['quick', 'quarry', 'quails']
+        )
+    );
 
-  it('Exercise 2', function(){
-    assert.deepEqual(E.filterQs(['quick', 'camels', 'quarry', 'over', 'quails']), ['quick', 'quarry', 'quails']);
-  });
+    it(
+        'Exercise 3', 
+        () => assert.equal(
+            E.max([323,523,554,123,5234]), 
+            5234
+        )
+    );
 
-  it('Exercise 3', function(){
-    assert.equal(E.max([323,523,554,123,5234]), 5234);
-  });
+    if (E.slice != undefined) {
+        it(
+            'Curry Bonus 1', 
+            () => assert.deepEqual(
+                E.slice(1)(3)(['a', 'b', 'c']), 
+                ['b', 'c']
+            )
+        );
+    }
 
-  if (E.slice != undefined) {
-    it('Curry Bonus 1', function(){
-      assert.deepEqual(E.slice(1)(3)(['a', 'b', 'c']), ['b', 'c']);
-    });
-  }
-
-  if (E.take != undefined) {
-    it('Curry Bonus 2', function(){
-      assert.deepEqual(E.take(2)(['a', 'b', 'c']), ['a', 'b']);
-    });
-  }
-
+    if (E.take != undefined) {
+        it(
+            'Curry Bonus 2', 
+            () => assert.deepEqual(
+                E.take(2)(['a', 'b', 'c']), 
+                ['a', 'b']
+            )
+        );
+    }
 });
