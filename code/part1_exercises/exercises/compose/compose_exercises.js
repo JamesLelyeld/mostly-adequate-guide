ad764680874a43d8bb453b54619534e10c46c5b1
James Lelyveld <james.lelyveld@tribalworldwide.co.uk>
2017-01-23 12:53:03 +0000

    Compose exercise

--- code/part1_exercises/exercises/compose/compose_exercises.js
+++ code/part1_exercises/exercises/compose/compose_exercises.js
@@ -2,6 +2,8 @@ require('../../support');
 var _ = require('ramda');
 var accounting = require('accounting');
   
+"use strict"
+
 // Example Data
 var CARS = [
     {name: "Ferrari FF", horsepower: 660, dollar_value: 700000, in_stock: true},
@@ -15,58 +17,67 @@ var CARS = [
 // Exercise 1:
 // ============
 // use _.compose() to rewrite the function below. Hint: _.prop() is curried.
-var isLastInStock = function(cars) {
-  var reversed_cars = _.last(cars);
-  return _.prop('in_stock', reversed_cars)
-};
+// var isLastInStock = function(cars) {
+//   var reversed_cars = _.last(cars);
+//   return _.prop('in_stock', reversed_cars)
+// };
+
+const isLastInStock = _.compose(_.prop('in_stock'), _.last);
+
 
 // Exercise 2:
 // ============
 // use _.compose(), _.prop() and _.head() to retrieve the name of the first car
-var nameOfFirstCar = undefined;
+const nameOfFirstCar = _.compose(_.prop('name'), _.head);
 
 
 // Exercise 3:
 // ============
 // Use the helper function _average to refactor averageDollarValue as a composition
-var _average = function(xs) { return reduce(add, 0, xs) / xs.length; }; // <- leave be
+const _average = function(xs) { return reduce(add, 0, xs) / xs.length; }; // <- leave be
 
-var averageDollarValue = function(cars) {
-  var dollar_values = map(function(c) { return c.dollar_value; }, cars);
-  return _average(dollar_values);
-};
-
+const averageDollarValue = _.compose(_average,  map((c) => c.dollar_value));
 
 // Exercise 4:
 // ============
 // Write a function: sanitizeNames() using compose that takes an array of cars and returns a list of lowercase and underscored names: e.g: sanitizeNames([{name: "Ferrari FF"}]) //=> ["ferrari_ff"].
 
-var _underscore = replace(/\W+/g, '_'); //<-- leave this alone and use to sanitize
+const _underscore = replace(/\W+/g, '_'); //<-- leave this alone and use to sanitize
 
-var sanitizeNames = undefined;
+const sanitizeNames = _.compose(map((c) => c.toLowerCase()), map(_underscore), map(_.prop('name')));
 
 
 // Bonus 1:
 // ============
 // Refactor availablePrices with compose.
 
-var availablePrices = function(cars) {
-  var available_cars = _.filter(_.prop('in_stock'), cars);
-  return available_cars.map(function(x){
-    return accounting.formatMoney(x.dollar_value)
-  }).join(', ');
+// var availablePrices = function(cars) {
+//   var available_cars = _.filter(_.prop('in_stock'), cars);
+//   return available_cars.map(function(x){
+//     return accounting.formatMoney(x.dollar_value)
+//   }).join(', ');
+// };
+
+const id = (x) => {
+    console.log('id', x);
+    return x;
+    
 };
 
+const availablePrices = _.compose(join(', ') , map((x)=> accounting.formatMoney(x.dollar_value)) , _.filter(_.prop('in_stock')));
+    
 
 // Bonus 2:
 // ============
 // Refactor to pointfree. Hint: you can use _.flip()
 
-var fastestCar = function(cars) {
-  var sorted = _.sortBy(function(car){ return car.horsepower }, cars);
-  var fastest = _.last(sorted);
-  return fastest.name + ' is the fastest';
-};
+// var fastestCar = function(cars) {
+//   var sorted = _.sortBy(function(car){ return car.horsepower }, cars);
+//   var fastest = _.last(sorted);
+//   return fastest.name + ' is the fastest';
+// };
+
+const fastestCar = _.compose(_.flip((x, y) => x + y)(' is the fastest'), _.prop('name'), _.last , _.sortBy(function(car){ return car.horsepower }) );
 
 
 module.exports = { CARS: CARS,
