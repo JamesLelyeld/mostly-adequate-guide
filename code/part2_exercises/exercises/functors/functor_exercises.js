5ecfaa84a9d5ffc6e5e88c79558105d9e4e4ceb4
James Lelyveld <james.lelyveld@tribalworldwide.co.uk>
2017-01-25 15:10:18 +0000

    Completing functor exercises.

--- code/part2_exercises/exercises/functors/functor_exercises.js
+++ code/part2_exercises/exercises/functors/functor_exercises.js
@@ -2,31 +2,35 @@ require('../../support');
 var Task = require('data.task');
 var _ = require('ramda');
 
+"use strict";
+
+const trace = (x) => {
+    console.log(x);
+    return x;
+};
+
 // Exercise 1
 // ==========
 // Use _.add(x,y) and _.map(f,x) to make a function that increments a value inside a functor
 
-var ex1 = undefined;
-
-
+const ex1 = map(_.add(1));
 
 // Exercise 2
 // ==========
 // Use _.head to get the first element of the list
-var xs = Identity.of(['do', 'ray', 'me', 'fa', 'so', 'la', 'ti', 'do']);
+const xs = Identity.of(['do', 'ray', 'me', 'fa', 'so', 'la', 'ti', 'do']);
 
-var ex2 = undefined;
-
+const ex2 = map(_.head());
 
 
 // Exercise 3
 // ==========
 // Use safeProp and _.head to find the first initial of the user
-var safeProp = _.curry(function (x, o) { return Maybe.of(o[x]); });
+const safeProp = _.curry(function (x, o) { return Maybe.of(o[x]); });
 
-var user = { id: 2, name: "Albert" };
+const user = { id: 2, name: "Albert" };
 
-var ex3 = undefined;
+const ex3 = _.compose(map(_.head), safeProp("name"));
 
 
 
@@ -34,12 +38,11 @@ var ex3 = undefined;
 // ==========
 // Use Maybe to rewrite ex4 without an if statement
 
-var ex4 = function (n) {
-  if (n) { return parseInt(n); }
-};
+// const ex4 = function (n) {
+//   if (n) { return parseInt(n); }
+// };
 
-var ex4 = undefined;
-
+const ex4 = _.compose(map(parseInt),Maybe.of);
 
 
 // Exercise 5
@@ -47,7 +50,7 @@ var ex4 = undefined;
 // Write a function that will getPost then _.toUpper the post's title
 
 // getPost :: Int -> Future({id: Int, title: String})
-var getPost = function (i) {
+const getPost = function (i) {
   return new Task(function(rej, res) {
     setTimeout(function(){
       res({id: i, title: 'Love them futures'})  
@@ -55,7 +58,9 @@ var getPost = function (i) {
   });
 };
 
-var ex5 = undefined;
+const getTitle = _.compose(_.toUpper, _.prop("title"));
+const ex5 = _.compose(map(getTitle) ,getPost);
+
 
 
 
@@ -63,13 +68,13 @@ var ex5 = undefined;
 // ==========
 // Write a function that uses checkActive() and showWelcome() to grant access or return the error
 
-var showWelcome = _.compose(_.concat( "Welcome "), _.prop('name'));
+const showWelcome = _.compose(_.concat( "Welcome "), _.prop('name'));
 
-var checkActive = function(user) {
+const checkActive = function(user) {
  return user.active ? Right.of(user) : Left.of('Your account is not active')
 };
 
-var ex6 = undefined;
+const ex6 = _.compose(map(showWelcome), checkActive);
 
 
 
@@ -77,8 +82,8 @@ var ex6 = undefined;
 // ==========
 // Write a validation function that checks for a length > 3. It should return Right(x) if it is greater than 3 and Left("You need > 3") otherwise
 
-var ex7 = function(x) {
-  return undefined; // <--- write me. (don't be pointfree)
+const ex7 = function(x) {
+  return x.length > 3 ? Right.of(x) : Left.of("You need > 3"); // <--- write me. (don't be pointfree)
 };
 
 
@@ -87,13 +92,15 @@ var ex7 = function(x) {
 // ==========
 // Use ex7 above and either as a functor to save the user if they are valid or return the error message string. Remember either's two arguments must return the same type.
 
-var save = function(x) {
+const save = function(x) {
   return new IO(function() {
     console.log("SAVED USER!");
     return x + '-saved';
   });
 };
 
-var ex8 = undefined;
+// e left right
+const ex8 = _.compose(either(IO.of, save), ex7);
+
 
 module.exports = {ex1: ex1, ex2: ex2, ex3: ex3, ex4: ex4, ex5: ex5, ex6: ex6, ex7: ex7, ex8: ex8};
